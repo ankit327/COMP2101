4a6d8a068093fae9aa9ed9d89444a88daee39244
Ankit <you@yourdomain>
2020-06-17 17:48:40 -0400

    Lab1 finally done

--- bash/helloworld.sh
+++ bash/helloworld.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "Hello world!"
-exit
+
+# My first script - helloworld.#!/bin/sh
+
+
+echo 'Hello World!'
+echo "I am process # $$"
---
+++ bash/helloworldtemplated.sh
@@ -0,0 +1,8 @@
+#!/bin/#!/usr/bin/env bash
+
+# My second script - helloworldtemplated.#!/bin/sh
+
+cat <<EOF
+Hello World!
+I am process # $$
+EOF
